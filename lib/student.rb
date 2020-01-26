6537572b1104024efe5f56b6f45e8c2f1aad9348
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 04:27:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,7 @@ class Student
     
     db[:conn].execute(sql, self.name, self.grade)
     
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
   def self.create(:name,:grade)
