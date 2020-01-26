2345e49921fe3dc2b441db51d9a02643aeec4cb2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 04:11:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,9 @@ class Student
   def self.create_table 
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students(
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    name TEXT 
+    grade INTEGER 
     )
     SQL
     
