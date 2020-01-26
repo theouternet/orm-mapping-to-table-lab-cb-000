8427ac7beb00a3002c29728eb53804e224c446a9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 04:29:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
   def save 
     sql = <<-SQL
     INSERT INTO students (name, grade) 
-    VALUES(?,?)
+    VALUES (?,?)
     SQL
     
     db[:conn].execute(sql, self.name, self.grade)
