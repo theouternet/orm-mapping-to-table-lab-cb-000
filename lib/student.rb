5187d1434a008c83bd25af6b0956e848a4c3f5a2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 03:57:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,31 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor 
+  attr_reader :id, 
+  
+  def initialize
+    
+  end 
+  
+  
+  def self.create_table 
+    
+  end 
+  
+  
+  def self.drop_table 
+    
+  end 
+  
+  
+  def save 
+    
+  end 
+  
+  
+  def self.create 
+    
+  end
+  
 end
